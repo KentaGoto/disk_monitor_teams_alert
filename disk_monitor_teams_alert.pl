c9dc6de7a13ad937f0e816075bfa9b9cdfe4ca17
KentaGoto <kenta-goto@to-in.co.jp>
2023-03-23 11:12:18 +0900

    English translation of comments

--- disk_monitor_teams_alert.pl
+++ disk_monitor_teams_alert.pl
@@ -5,17 +5,17 @@ use LWP::UserAgent;
 use Sys::Hostname;
 
 
-# 監視対象のドライブと警告のしきい値を設定
+# Set monitored drives and warning thresholds.
 my $drive_to_monitor = 'C:';
 my $threshold_percentage = 95;
-my $teams_webhook_url = 'https://365toin.webhook.office.com/'; # ここにWebhook URLを設定
+my $teams_webhook_url = 'https://365toin.webhook.office.com/'; # Webhook URL
 
-# ホスト名
+# Hostname
 sub get_hostname {
     return hostname;
 }
 
-# Teamsにメッセージを投稿
+# Send to Teams message.
 sub send_to_teams {
     my $message = shift;
     my $ua = LWP::UserAgent->new;
@@ -30,7 +30,7 @@ sub send_to_teams {
     $ua->request($req);
 }
 
-# ディスク使用率をチェック
+# Check disk usage.
 sub check_disk_space {
     my $output = `powershell -Command "Get-Volume -DriveLetter '$drive_to_monitor' | ForEach-Object { Write-Output ('Size:' + \$_.Size + ';FreeSpace:' + \$_.SizeRemaining) }"`;
 
@@ -38,7 +38,7 @@ sub check_disk_space {
         my $total_bytes = int($1);
         my $free_bytes = int($2);
 
-        # 使用率を計算
+        # Calculate utilization.
         my $used_bytes = $total_bytes - $free_bytes;
         my $usage_percentage = int($used_bytes / $total_bytes * 100);
 
@@ -48,11 +48,11 @@ sub check_disk_space {
     }
 }
 
-# ディスク使用率を監視
+# Monitor disk utilization.
 while (1) {
     my ($usage_percentage, $total_bytes, $free_bytes) = check_disk_space();
 
-    # 使用率がしきい値を超えているかどうかを判定
+    # Determine if utilization exceeds thresholds.
     if ($usage_percentage > $threshold_percentage) {
         my $message = get_hostname();
         $message .= "\n";
@@ -63,6 +63,6 @@ while (1) {
         print "Disk usage for '$drive_to_monitor' is at $usage_percentage% (Threshold: $threshold_percentage%)\n";
     }
 
-    # 1分間スリープ
+    # Sleep for 1 minute.
     sleep(60);
 }
